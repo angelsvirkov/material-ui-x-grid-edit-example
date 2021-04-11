7aac5b70e4992502890a2ae4021b381223f11ae2
Angel Svirkov <angel.svirkov@trivago.com>
2021-04-11 11:38:41 +0200

    feat(demo): Add full controlled edit component

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,70 +1,237 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Rating } from "@material-ui/lab";
-import { DataGrid, GridCellParams } from "@material-ui/data-grid";
-import { renderRating } from "@material-ui/x-grid-data-generator";
+import {
+  GridCellParams,
+  GridColumns,
+  GridRowsProp,
+  useGridApiRef,
+  GridEditRowModelParams,
+  XGrid,
+} from "@material-ui/x-grid";
+import {
+  Button,
+  Checkbox,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@material-ui/core";
 
 const useStyles = makeStyles({
-  root: {
-    display: "inline-flex",
-    flexDirection: "row",
-    alignItems: "center",
-    height: 48,
-    paddingLeft: 20
-  }
+  checkbox: {
+    // centering the checkbox inside the cell
+    margin: "0 auto",
+  },
 });
-export function GridRatingEditInputCell(props: GridCellParams) {
-  const { id, value, api, field } = props;
+
+const CheckboxCell: (
+  params: GridCellParams
+) => React.ReactElement<GridCellParams, any> = ({
+  id,
+  field,
+  api,
+  cellMode,
+  value,
+}) => {
   const classes = useStyles();
 
   const handleChange = React.useCallback(
-    (event) => {
-      const editProps = {
-        value: Number(event.target.value)
-      };
-      api.commitCellChange({ id, field, props: editProps });
-
-      // If we do not change the cell mode, then the cell commit change
-      // is not triggerring any change to the edit mode component
-      // api.setCellMode(id, field, 'view');
+    (event, value) => {
+      api.setEditCellProps({
+        id,
+        field,
+        props: {
+          value: Number(value),
+        },
+      });
     },
     [api, field, id]
   );
 
   return (
-    <div className={classes.root}>
-      <Rating value={Number(value)} onChange={handleChange} />
-    </div>
+    <Checkbox
+      classes={{ root: classes.checkbox }}
+      checked={!!value}
+      disabled={cellMode === "view"}
+      onChange={handleChange}
+    />
   );
-}
+};
 
-function renderRatingEditInputCell(params) {
-  return <GridRatingEditInputCell {...params} />;
-}
-const columns = [
+const columns: GridColumns = [
   {
-    field: "places",
-    headerName: "Places",
-    width: 120
+    field: "country",
+    headerName: "Country",
+    width: 120,
+    editable: false,
+    align: "center",
   },
   {
-    field: "rating",
-    headerName: "Rating",
-    renderCell: renderRating,
-    renderEditCell: renderRatingEditInputCell,
+    field: "status1",
+    headerName: "Status 1",
+    type: "number",
+    width: 120,
     editable: true,
-    width: 180,
-    type: "number"
-  }
+    renderEditCell: CheckboxCell,
+    renderCell: CheckboxCell,
+  },
+  {
+    field: "status2",
+    headerName: "Status 2",
+    type: "number",
+    width: 120,
+    editable: true,
+    renderEditCell: CheckboxCell,
+    renderCell: CheckboxCell,
+    align: "center",
+  },
+  {
+    field: "status3",
+    headerName: "Status 3",
+    type: "number",
+    width: 120,
+    editable: true,
+    renderEditCell: CheckboxCell,
+    renderCell: CheckboxCell,
+    align: "center",
+  },
 ];
 
-const rows = [
-  { id: 1, places: "Barcelona", rating: 5 },
-  { id: 2, places: "Rio de Janeiro", rating: 4 },
-  { id: 3, places: "London", rating: 3 },
-  { id: 4, places: "New York", rating: 2 }
+const initialRows: GridRowsProp = [
+  {
+    id: "DE",
+    country: "DE",
+    status1: 1,
+    status2: 0,
+    status3: 0,
+  },
+  {
+    id: "UK",
+    country: "UK",
+    status1: 0,
+    status2: 1,
+    status3: 0,
+  },
+  {
+    id: "ES",
+    country: "ES",
+    status1: 0,
+    status2: 0,
+    status3: 1,
+  },
 ];
 
-export default function CustomRatingOperator() {
-  return <DataGrid rows={rows} columns={columns} autoHeight hideFooter />;
-}
+type OwnCell = {
+  // Represents the row
+  id: string;
+  // Represents the column
+  field: string;
+};
+
+const editableColumns = columns.filter(column => column.editable);
+const allEditableCells: OwnCell[] = initialRows.reduce<OwnCell[]>(
+  (prevAllCells, currentRow) => {
+    for (const column of editableColumns) {
+      prevAllCells.push({
+        id: currentRow.id,
+        field: column.field,
+      });
+    }
+    return prevAllCells;
+  },
+  []
+);
+
+const Demo: React.FC<{}> = () => {
+  const [isEditing, setIsEditing] = React.useState(false);
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+
+  const [editRowsModel, setEditRowsModel] = React.useState({});
+  const [rows, setRows] = React.useState<GridRowsProp>([]);
+
+  const apiRef = useGridApiRef();
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setRows(initialRows);
+    }, 1500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleEditRowModelChange = React.useCallback(
+    (params: GridEditRowModelParams) => {
+      setEditRowsModel(params.model);
+    },
+    []
+  );
+
+  const handleEditClick = () => {
+    if (!isEditing) {
+      for (const cell of allEditableCells) {
+        apiRef.current.setCellMode(cell.id, cell.field, "edit");
+      }
+      setIsEditing(true);
+    } else {
+      setIsDialogOpen(true);
+    }
+  };
+
+  const handleCloseDialog = () => {
+    for (const cell of allEditableCells) {
+      apiRef.current.setCellMode(cell.id, cell.field, "view");
+    }
+    setIsEditing(false);
+    setIsDialogOpen(false);
+  };
+
+  const stopEventPropagation = React.useCallback(
+    (params: GridCellParams, event: React.SyntheticEvent) => {
+      event.stopPropagation();
+    },
+    []
+  );
+
+  return (
+    <div>
+      <XGrid
+        apiRef={apiRef}
+        loading={!rows.length}
+        rows={rows}
+        columns={columns}
+        editRowsModel={editRowsModel}
+        onEditRowModelChange={handleEditRowModelChange}
+        autoHeight
+        hideFooter={true}
+        // Overriding the default behavior
+        onCellBlur={stopEventPropagation}
+        onCellKeyDown={stopEventPropagation}
+        onCellClick={stopEventPropagation}
+        onCellDoubleClick={stopEventPropagation}
+      />
+
+      <Dialog
+        open={isDialogOpen}
+        onClose={() => {
+          setIsDialogOpen(false);
+        }}
+      >
+        <DialogTitle>Changes</DialogTitle>
+        <DialogContent>
+          <code>{JSON.stringify(editRowsModel)}</code>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <br />
+      <Button variant="outlined" onClick={handleEditClick}>
+        {isEditing ? "Save" : "Edit"}
+      </Button>
+    </div>
+  );
+};
+
+export default Demo;
